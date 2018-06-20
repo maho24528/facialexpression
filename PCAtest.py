4fe9dba7a09742b1769e7d90c565672989fdc5a1
maho24528 <40426689+maho24528@users.noreply.github.com>
2018-06-20 21:24:52 +0900

    Update PCAtest.py

--- PCAtest.py
+++ PCAtest.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#未完成
 import cv2
 import openface
 import os
@@ -10,13 +11,8 @@ v=[]
 align = openface.AlignDlib("../openface/models/dlib/shape_predictor_68_face_landmarks.dat")
 
 #import image
-
 pic = glob.glob('/home/docker/CK+image/*.png')
-"""
-pic=sorted(os.listdir("/home/docker/CK+image"))
-pic.pop(0)
-print(pic[:5])
-"""
+
 for p in pic:
     img=cv2.imread(p)
     bb = align.getLargestFaceBoundingBox(img)
@@ -25,15 +21,14 @@ for p in pic:
     aligned_face = align.align(64, img, bb, landmarks)
     aligned_face=cv2.cvtColor(aligned_face, cv2.COLOR_RGB2GRAY)
     aligned_face=cv2.equalizeHist(aligned_face)
-    i=0
 
-    #ベクトルを出す
+ #各画像についてベクトルを出す
     for a in aligned_face:
         for aa in a:
             v.append(aa)
         vector.append(v)
     
-#PCA
+#PCAを行う
 from sklearn.decomposition import PCA
 pca=PCA(n_components=10)
 pca.fit(vector)
